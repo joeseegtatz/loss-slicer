84e9bb0f3bd48a35ace596367e2c4a333f863526
joeseegtatz <joe2022@live.de>
2025-06-24 22:15:55 +0200

    removed unused file

--- pysclice/core/__init__.py
+++ pysclice/core/__init__.py
@@ -3,12 +3,10 @@
 Core components for loss landscape analysis.
 """
 from .model_wrapper import ModelWrapper
-from .slicing_context import SlicingContext
 from .parameter_vector import *
 
 __all__ = [
     "ModelWrapper",
-    "SlicingContext",
     "normalize_direction",
     "random_direction",
     "filter_normalized_directions",
